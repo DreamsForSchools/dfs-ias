7a52f01e8f8b2383823bec8574a4324b9efa11d8
Tyler Yin <tyler.yin321@gmail.com>
2020-04-22 21:49:23 -0700

    Build folder

---
+++ build/precache-manifest.3bb01d7015ab3f438e7c2bfa00774c64.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac0906486cacaddf0cb698653d902038",
+    "url": "/index.html"
+  },
+  {
+    "revision": "915ec98817501f1db408",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3c0686b89369bcdcf651",
+    "url": "/static/js/2.6dda5a9f.chunk.js"
+  },
+  {
+    "revision": "78aa0676509153305bde072ed1e9cb1c",
+    "url": "/static/js/2.6dda5a9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "915ec98817501f1db408",
+    "url": "/static/js/main.ddd29d72.chunk.js"
+  },
+  {
+    "revision": "3eba1d4bda0114daa469",
+    "url": "/static/js/runtime-main.f74f512b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
