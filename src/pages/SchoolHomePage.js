08c833e0418c9e8377d2361b74244b9ca0ccf7c1
Hannah Fragante <hfragant@uci.edu>
2020-05-27 06:41:01 -0700

    added "check existing roster" in home pages

--- src/pages/SchoolHomePage.js
+++ src/pages/SchoolHomePage.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import add from '.././assets/add.svg'
+import groupIcon2 from '.././assets/groupIcon2.svg';
 
 import SideNavBar from '../components/sidebar/SideNavBar';
 import { useHistory } from "react-router-dom";
@@ -16,6 +17,19 @@ export default function SchoolHomePage() {
         fire.auth().onAuthStateChanged(user => {
             if (user){
                 history.push('/schoolhome/addrosterform');
+            }else{
+                history.push('/');
+            }
+            
+          })
+
+    }
+
+    const currentRoster = () => {
+        fire.auth().onAuthStateChanged(user => {
+            if (user){
+                history.push('/schoolhome/addrosterform');
+                // history.push('/appjamhome/roster');
             }else{
                 history.push('/');
             }
@@ -44,13 +58,26 @@ export default function SchoolHomePage() {
 
                 <div className="hozLineDivider"></div>
 
-                <button onClick={addRoster} className="programPageAddRosterBtn">
-                    <img src={add} className="addLogo" alt="logo"/>
-                    <h4 className="addRosterLaber">Add Roster</h4>
-                </button>
+                <div style={buttonsContainer}>
+                    <button onClick={currentRoster} className="programPageAddRosterBtn1">
+                        <img src={groupIcon2} className="addLogo1" alt="logo"/>
+                        <h4 className="addRosterLaber1">Check Existing Roster</h4>
+                    </button>
+
+                    <button onClick={addRoster} className="programPageAddRosterBtn">
+                        <img src={add} className="addLogo" alt="logo"/>
+                        <h4 className="addRosterLaber">New Roster</h4>
+                    </button>
+                </div>
 
             </div>
 
         </div>
     )
+}
+
+const buttonsContainer = {
+    display: "flex",
+    flexDirection: "row",
+    marginLeft: "51px"
 }
