25ff5fef1120123e83932cba6bc57662e2cb6f9f
Hannah Fragante <hfragant@uci.edu>
2020-06-09 05:31:17 -0700

    updated shirt pages

--- src/pages/sortingPages/sphero/SpheroShirtPage.js
+++ src/pages/sortingPages/sphero/SpheroShirtPage.js
@@ -52,10 +52,21 @@ export default function SpheroShirtPage() {
     },[]);
 
     //gets the appjam shirt size info fromt he database using firebase
-    const sortedRosterCollection = useRef(fire.database().ref().child('shirts'))
+    const sortedRosterCollection = useRef(fire.database().ref().child('SpheroElementary/shirts'))
 
     //gets the info from firebase
     useEffect(() => {
+        fetch('http://apurva29.pythonanywhere.com/shirts', {
+            method: 'POST', // or 'PUT'
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({"Program":"SpheroElementary"}),
+        })
+        .then(response => response.json())
+        
+
         sortedRosterCollection.current.once('value', (snap) => {
             const shirtSizes = {}
             // const roster = []
