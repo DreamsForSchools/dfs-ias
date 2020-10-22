cf49fba25676d037876900d19c2f6fbbe19ae050
Doankt <36061217+Doankt@users.noreply.github.com>
2020-10-22 13:52:43 -0700

    Create fbdelete.py

---
+++ server/src/fbdelete.py
@@ -0,0 +1,15 @@
+import pyrebase
+import dfsapi
+import calendar
+import time
+import dbtools
+from collections import defaultdict
+
+def delete_institution():
+    pass
+
+def delete_instructor():
+    pass
+
+def delete_program():
+    pass
