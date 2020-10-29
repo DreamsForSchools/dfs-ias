a4d14f0bca450901e791ea957caf3a414c3ab295
Doankt <36061217+Doankt@users.noreply.github.com>
2020-10-28 19:27:03 -0700

    Added temp prints for nonexistent keys

--- server/src/fbdelete.py
+++ server/src/fbdelete.py
@@ -9,17 +9,26 @@ from collections import defaultdict
 def delete_instructor(teacher: dict):
     db = dfsapi.get_db()
     pk = teacher["Name"] + "," + teacher["Major"] + "," + teacher["University"]
+
+    data = db.child(teacher["Season"]).child("Instructors").child(pk).get()
+    if not data.val():  print("EXCEPTION HERE (Path not found)")
+
     db.child(teacher["Season"]).child("Instructors").child(pk).remove()
 
 def delete_program(program:dict):
     db = dfsapi.get_db()
+
+    data = db.child("Programs").child(program["Name"]).get()
+    if not data.val():  print("EXCEPTION HERE (Path not found)")
+
     db.child("Programs").child(program["Name"]).remove()
+
 
 def delete_institution(season: str, inst_key: str):
     db = dfsapi.get_db()
     
     data = db.child(season).child("institutions").child(inst_key).get()
-    if not data.val(): print("EXCEPTION HERE (Path not found)")
+    if not data.val():  print("EXCEPTION HERE (Path not found)")
 
     db.child(season).child("institutions").child(inst_key).remove()
 
