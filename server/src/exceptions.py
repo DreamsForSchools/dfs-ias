3c7e89937f741a6b4a0bc06e4ad993f7a0bc51c8
Doankt <36061217+Doankt@users.noreply.github.com>
2020-10-30 19:29:19 -0700

    Create KeyExists exception

--- server/src/exceptions.py
+++ server/src/exceptions.py
@@ -2,3 +2,8 @@ class KeyNotFoundError(Exception):
     def __init__(self, key):
         self.key = key
         super().__init__("Key ({}) not found".format(key))
+
+class KeyExists(Exception):
+    def __init__(self, key):
+        self.key = key
+        super().__init__("Key ({}) already exists".format(key))
