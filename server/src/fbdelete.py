eac91722ac36838de5ff9618471fe050b398c402
Doankt <36061217+Doankt@users.noreply.github.com>
2020-11-01 13:13:14 -0800

    Fixing exception handling

--- server/src/fbdelete.py
+++ server/src/fbdelete.py
@@ -6,7 +6,7 @@ import dbtools
 from collections import defaultdict
 from exceptions import KeyNotFoundError
 
-def delete_instructor(teacher: dict):
+def delete_instructor(season: str, teacher: dict):
     db = dfsapi.get_db()
     pk = teacher["Name"] + "," + teacher["Major"] + "," + teacher["University"]
 
@@ -19,22 +19,23 @@ def delete_school(season: str, school_key: str):
     db = dfsapi.get_db()
     
     data = db.child(season).child("schools").child(school_key).get()
-    if not data.val() is None:  raise KeyNotFoundError("dfs-ias/{s}/schools/{name}".format(s=season,name=school_key))
+    if data.val() is None:  raise KeyNotFoundError("dfs-ias/{s}/schools/{name}".format(s=season,name=school_key))
 
     db.child(season).child("schools").child(school_key).remove()
 
-def delete_program(program:dict):
+def delete_program(season: str, program:str):
     db = dfsapi.get_db()
 
-    data = db.child("programs").child(program["Name"]).get()
-    if not data.val() is None:  raise KeyNotFoundError("temp path")
+    data = db.child(season).child("programs").child(program).get()
+    if data.val() is None:  raise KeyNotFoundError("dfs-ias/{s}/programs/{p}".format(s=season, p=program))
 
-    db.child("programs").child(program["Name"]).remove()
+    db.child(season).child("programs").child(program).remove()
 
 if __name__ == "__main__":
     print("Beginning Tests")
     try:
-        delete_school("fall2020", "bad school")
+        # delete_school("fall2020", "school3")
+        delete_program("fall2020", "AJ2")
     except KeyNotFoundError as err:
         print(err)
     print("Ending Tests")
