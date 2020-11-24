6d3f7ae8f88e05ebb5749f057f63270889de2f85
Doankt <36061217+Doankt@users.noreply.github.com>
2020-11-23 16:10:35 -0800

    Remove most test lines

--- server/src/dfsgmap.py
+++ server/src/dfsgmap.py
@@ -3,26 +3,18 @@ GMAP_API_KEY = ''
 import googlemaps
 CLIENT = googlemaps.Client(GMAP_API_KEY)
 
-def distance_between(addr_origin: str, addr_dest: str):
-	ret_dict = dict()
+def distance_between(addr_origin: str, addr_dest: list or str):
 	res = CLIENT.distance_matrix(
 		origins= addr_origin,
 		destinations= addr_dest,
 		mode= "driving"
 	)
 
-	# for k, v in res.items():
-	# 	print(k, ": ", v)
-	# print(res['rows'][0]['elements'][0]['distance'])
-	# print(res['rows'][0]['elements'][0]['distance']['value'])
-	# print()
+	ret_dict = dict()
+	for addr, row in zip(addr_origin, res['rows']):
+		ret_dict[addr] = row['elements'][0]['distance']['value']
 
-	# for row in res['rows']:
-	# 	print(row['elements'][0]['distance']['value'])
-
-
-	# return res['rows'][0]['elements'][0]['distance']['value']
-	return 0
+	return ret_dict
 
 if __name__ == "__main__":
 	pass
