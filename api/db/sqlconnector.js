cb46220c78388b803ff9803287095236574175af
Mars <32443134+uci-mars@users.noreply.github.com>
2021-02-10 18:55:13 -0800

    mysql goes brrrrrr

--- api/db/sqlconnector.js
+++ api/db/sqlconnector.js
@@ -2,14 +2,18 @@ const mysql = require('mysql2');
 
 const db = mysql.createConnection({
     host: 'localhost',
+    port: '3306',
     user: 'root',
-    password: 'root',
-    database: 'dfs'
+    password: 'root'
 })
 
 db.connect(function(err) {
-    if (err) throw err;
-    console.log("Database Connected!");
+    if (err) {
+      console.error("error connecting");
+      return null;
+    } else {
+      console.log("Database Connected!");
+    }
   });
 
 module.exports = db;
