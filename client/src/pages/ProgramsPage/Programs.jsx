b80dc062819ff4ec81e2fc0e5736bbd3c64e8172
Mars <32443134+uci-mars@users.noreply.github.com>
2021-02-24 23:59:47 -0800

    started the card and program page component

--- client/src/pages/ProgramsPage/Programs.jsx
+++ client/src/pages/ProgramsPage/Programs.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import OptionsBar from './OptionsBar';
 import './Programs.scss';
+import Card from '../../components/Card';
+
+const DUMMY_DATA = [
+    {name: "AppJam", color: "black", logo: "https://media-exp1.licdn.com/dms/image/C560BAQEqVUF6hTLzaQ/company-logo_200_200/0/1547068849793?e=2159024400&v=beta&t=579oZ0aNyoxEmS-txIZaLlDsPd-GVR03zpmsObt9Iy0"},
+    {name: "WebJam", color: "red", logo: "https://media-exp1.licdn.com/dms/image/C560BAQEqVUF6hTLzaQ/company-logo_200_200/0/1547068849793?e=2159024400&v=beta&t=579oZ0aNyoxEmS-txIZaLlDsPd-GVR03zpmsObt9Iy0"},
+    {name: "LESTEM", color: "green", logo: "https://media-exp1.licdn.com/dms/image/C560BAQEqVUF6hTLzaQ/company-logo_200_200/0/1547068849793?e=2159024400&v=beta&t=579oZ0aNyoxEmS-txIZaLlDsPd-GVR03zpmsObt9Iy0"},
+    {name: "Engineering Inventors", color: "purple", logo: "https://media-exp1.licdn.com/dms/image/C560BAQEqVUF6hTLzaQ/company-logo_200_200/0/1547068849793?e=2159024400&v=beta&t=579oZ0aNyoxEmS-txIZaLlDsPd-GVR03zpmsObt9Iy0"},
+    {name: "Scratch", color: "yellow", logo: "https://media-exp1.licdn.com/dms/image/C560BAQEqVUF6hTLzaQ/company-logo_200_200/0/1547068849793?e=2159024400&v=beta&t=579oZ0aNyoxEmS-txIZaLlDsPd-GVR03zpmsObt9Iy0"}
+]
 
 function Programs() {
   return (
-    <div className="programs">
-      Programs Page
+    <div className="programs_page">
+        <div className={"program-gallery_container"}>
+            <OptionsBar />
+            <div className={'program-gallery'}>
+                {
+                    DUMMY_DATA.map((el, index) =>
+                        <Card item={el} id={index}/>
+                    )
+                }
+            </div>
+        </div>
+        <div className={"program-side-info_container"}>
+
+
+        </div>
     </div>
   );
 }
