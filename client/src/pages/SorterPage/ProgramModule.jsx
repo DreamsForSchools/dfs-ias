14ff652d37f99b923365fec6c82ea300ca35ef1e
trishaprile <trishaprilee@gmail.com>
2021-03-02 03:51:06 -0800

    switch to accordion

--- client/src/pages/SorterPage/ProgramModule.jsx
+++ client/src/pages/SorterPage/ProgramModule.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import './ProgramModule.scss';
-import { Button } from "react-bootstrap";
+import { Accordion, Card } from "react-bootstrap";
 import { CaretRightFill, CaretDownFill, LockFill, UnlockFill } from 'react-bootstrap-icons';
 
 function ProgramModule({ name, color }) {
@@ -8,30 +8,54 @@ function ProgramModule({ name, color }) {
   const [lock, setLock] = useState(false);
 
   return (
-    <div
-      className="program-module"
-      style={{ backgroundColor: color }}
-      onClick={() => {
-        setShowContent(!showContent);
-      }}
-    >
-      <span className="title">
-        <span className="program-left">
-          {showContent ? <CaretDownFill className="icon" /> : <CaretRightFill className="icon" />}
-          <div className="program-name">{name}</div>
-        </span>
-        <span className="program-right" 
-          onClick={() => {
-            setLock(!lock);
-          }}
-        >
-          {lock ? <LockFill className="icon" /> : <UnlockFill className="icon" />}
-        </span>
-      </span>
-      {/* {showContent && (
-        <div className="program-content">Schools</div>
-      )} */}
-    </div>
+    <Accordion>
+      <Card className="program-module" style={{ backgroundColor: color }}>
+        <Card.Header>
+          <Accordion.Toggle className="program-header" onClick={() => {setShowContent(!showContent)}} as={Card.Header} eventKey="0">
+            <span className="program-left">
+              {showContent ? <CaretDownFill className="icon" /> : <CaretRightFill className="icon" />} 
+              <div className="program-name">{name}</div>
+            </span>
+            <span className="program-right" onClick={() => {setLock(!lock)}}>
+              {lock ? <LockFill className="icon" /> : <UnlockFill className="icon" />}
+            </span>
+          </Accordion.Toggle>
+        </Card.Header>
+        <Accordion.Collapse eventKey="0">
+          <Card.Body className="program-body">
+            Hello! I'm the body
+          </Card.Body>
+        </Accordion.Collapse>
+      </Card>
+    </Accordion>
+
+
+    // <div
+    //   className="program-module"
+    //   style={{ backgroundColor: color }}
+    //   onClick={() => {
+    //     setShowContent(!showContent);
+    //   }}
+    // >
+    //   <span className="title">
+    //     <span className="program-left">
+    //       {showContent ? <CaretDownFill className="icon" /> : <CaretRightFill className="icon" />}
+    //       <div className="program-name">{name}</div>
+    //     </span>
+    //     <span className="program-right" 
+    //       onClick={() => {
+    //         setLock(!lock);
+    //       }}
+    //     >
+    //       {lock ? <LockFill className="icon" /> : <UnlockFill className="icon" />}
+    //     </span>
+    //   </span>
+    //   {showContent && (
+    //     <div className="program-content">
+    //       Schools
+    //     </div>
+    //   )}
+    // </div>
   );
 }
 
