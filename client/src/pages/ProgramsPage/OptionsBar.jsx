adeaf6088a7504a1b77570652c1e43523ae55e7b
Arjun Tyagi <tyagi.arjun@gmail.com>
2021-03-02 18:21:42 -0800

    Created the programs page

--- client/src/pages/ProgramsPage/OptionsBar.jsx
+++ client/src/pages/ProgramsPage/OptionsBar.jsx
@@ -1,28 +1,133 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './OptionsBar.scss';
-import { Form } from 'react-bootstrap';
+import {Form, Dropdown, Button, Col, Row, Container} from 'react-bootstrap';
+import {MdAddCircleOutline} from 'react-icons/md';
 
-const OptionBar = () => {
+const OptionBar = (props) => {
+    const [viewValue, setViewValue] = useState("Programs");
+    const [filterValue, setFilterValue] = useState("All");
+
+    function handleView(e, props) {
+        setViewValue(e);
+        props.viewType(e);
+    }
+
+    function handleFilter(e, props) {
+        setFilterValue(e);
+        props.filterType(e);
+    }
+
     return (
-        <div className={"option-bar_container"}>
-            <Form.Label>View by: </Form.Label>
-            <Form.Control as="select">
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-            </Form.Control>
-            <Form.Label>Show: </Form.Label>
-            <Form.Control as="select">
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-            </Form.Control>
-        </div>
+            <div className={"option-bar-container"}>
+
+                <Container fluid>
+                    <Row>
+                        <Col xs={6} s={6} md={4} >
+                            <Dropdown className={"dropdown-view"}>
+                                <label className="dropdown-label-view">View By:</label>
+                                <Dropdown.Toggle id="selector">
+                                    {viewValue}
+                                </Dropdown.Toggle>
+
+                                <Dropdown.Menu className="selector-menu">
+                                    <Dropdown.Item as="button"
+                                                   onClick={(e) => handleView(e.target.textContent, props)}>Programs</Dropdown.Item>
+                                    <Dropdown.Item as="button"
+                                                   onClick={(e) => handleView(e.target.textContent, props)}>Partners</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
+                        </Col>
+
+                        <Col xs={6} s={6} md={5}>
+                            <Dropdown className={"dropdown-filter"}>
+                                <label className="dropdown-label-filter">Filter By:</label>
+                                <Dropdown.Toggle id="selector">
+                                    {filterValue}
+                                </Dropdown.Toggle>
+
+                                <Dropdown.Menu className="selector-menu">
+                                    <Dropdown.Item as="button"
+                                                   onClick={(e) => handleFilter(e.target.textContent, props)}>All</Dropdown.Item>
+                                    <Dropdown.Item as="button"
+                                                   onClick={(e) => handleFilter(e.target.textContent, props)}>DFS Programs</Dropdown.Item>
+                                    <Dropdown.Item as="button"
+                                                   onClick={(e) => handleFilter(e.target.textContent, props)}>Housing Community</Dropdown.Item>
+                                    <Dropdown.Item as="button"
+                                                   onClick={(e) => handleFilter(e.target.textContent, props)}>Non-profit Partners</Dropdown.Item>
+                                    <Dropdown.Item as="button"
+                                                   onClick={(e) => handleFilter(e.target.textContent, props)}>Public Schools</Dropdown.Item>
+                                    <Dropdown.Item as="button"
+                                                   onClick={(e) => handleFilter(e.target.textContent, props)}>Private Schools</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
+                        </Col>
+
+                        <Col xs={6} s={6} md={1} >
+                            <Button className={"add-button"}>Add Program <MdAddCircleOutline size={25} /></Button>
+                        </Col>
+
+                    </Row>
+                </Container>
+            </div>
     )
 }
 
 export default OptionBar;
+
+// <Container>
+//     <Row>
+//         <Col xs={6} md={4}>
+//             xs=6 md=4
+//         </Col>
+//         <Col xs={6} md={4}>
+//             xs=6 md=4
+//         </Col>
+//         <Col xs={6} md={4}>
+//             xs=6 md=4
+//         </Col>
+//     </Row>
+// </Container>
+
+//
+// <div className={"option-bar_container"}>
+//     <div className={"dropdowns"}>
+//         <Dropdown>
+//             <label className="dropdown-label">View By:</label>
+//             <Dropdown.Toggle id="view-selector">
+//                 {viewValue}
+//             </Dropdown.Toggle>
+//
+//             <Dropdown.Menu className="view-selector-menu">
+//                 <Dropdown.Item as="button"
+//                                onClick={(e) => setViewValue(e.target.textContent)}>Programs</Dropdown.Item>
+//                 <Dropdown.Item as="button"
+//                                onClick={(e) => setViewValue(e.target.textContent)}>Partners</Dropdown.Item>
+//             </Dropdown.Menu>
+//         </Dropdown>
+//         <Dropdown className={"dropdowns-2"}>
+//             <label className="dropdown-label-2">Filter By:</label>
+//             <Dropdown.Toggle id="view-selector">
+//                 {filterValue}
+//             </Dropdown.Toggle>
+//
+//             <Dropdown.Menu className="view-selector-menu">
+//                 <Dropdown.Item as="button"
+//                                onClick={(e) => setFilterValue(e.target.textContent)}>All</Dropdown.Item>
+//                 <Dropdown.Item as="button"
+//                                onClick={(e) => setFilterValue(e.target.textContent)}>DFS Programs</Dropdown.Item>
+//                 <Dropdown.Item as="button"
+//                                onClick={(e) => setFilterValue(e.target.textContent)}>Housing Communities</Dropdown.Item>
+//                 <Dropdown.Item as="button"
+//                                onClick={(e) => setFilterValue(e.target.textContent)}>Non-profit Partners</Dropdown.Item>
+//                 <Dropdown.Item as="button"
+//                                onClick={(e) => setFilterValue(e.target.textContent)}>Public Schools</Dropdown.Item>
+//                 <Dropdown.Item as="button"
+//                                onClick={(e) => setFilterValue(e.target.textContent)}>Private Schools</Dropdown.Item>
+//             </Dropdown.Menu>
+//         </Dropdown>
+//     </div>
+//     <div className={"buttons"}>
+//         <Button className={"add-button"}>Add Program <MdAddCircleOutline size={25} /></Button>
+//     </div>
+//
+// </div>
--- client/src/pages/ProgramsPage/Programs.jsx
+++ client/src/pages/ProgramsPage/Programs.jsx
@@ -1,34 +1,71 @@
-import React from 'react';
+import React, {useState} from 'react';
 import OptionsBar from './OptionsBar';
 import './Programs.scss';
+import './OptionsBar.scss';
 import Card from '../../components/Card';
+import AppJam_Logo from "../../assets/MobileDevLogo.png";
+import WebJam_Logo from "../../assets/WebDevLogo.png";
+import Lestem_Logo from "../../assets/LESTEMLogo.png";
+import Efk_Logo from "../../assets/EFKLogo.png";
+import Scratch_Logo from "../../assets/ScratchLogo.png";
 
-const DUMMY_DATA = [
-    {name: "AppJam", color: "black", logo: "https://media-exp1.licdn.com/dms/image/C560BAQEqVUF6hTLzaQ/company-logo_200_200/0/1547068849793?e=2159024400&v=beta&t=579oZ0aNyoxEmS-txIZaLlDsPd-GVR03zpmsObt9Iy0"},
-    {name: "WebJam", color: "red", logo: "https://media-exp1.licdn.com/dms/image/C560BAQEqVUF6hTLzaQ/company-logo_200_200/0/1547068849793?e=2159024400&v=beta&t=579oZ0aNyoxEmS-txIZaLlDsPd-GVR03zpmsObt9Iy0"},
-    {name: "LESTEM", color: "green", logo: "https://media-exp1.licdn.com/dms/image/C560BAQEqVUF6hTLzaQ/company-logo_200_200/0/1547068849793?e=2159024400&v=beta&t=579oZ0aNyoxEmS-txIZaLlDsPd-GVR03zpmsObt9Iy0"},
-    {name: "Engineering Inventors", color: "purple", logo: "https://media-exp1.licdn.com/dms/image/C560BAQEqVUF6hTLzaQ/company-logo_200_200/0/1547068849793?e=2159024400&v=beta&t=579oZ0aNyoxEmS-txIZaLlDsPd-GVR03zpmsObt9Iy0"},
-    {name: "Scratch", color: "yellow", logo: "https://media-exp1.licdn.com/dms/image/C560BAQEqVUF6hTLzaQ/company-logo_200_200/0/1547068849793?e=2159024400&v=beta&t=579oZ0aNyoxEmS-txIZaLlDsPd-GVR03zpmsObt9Iy0"}
-]
+
 
 function Programs() {
-  return (
-    <div className="programs_page">
-        <div className={"program-gallery_container"}>
-            <OptionsBar />
-            <div className={'program-gallery'}>
-                {
-                    DUMMY_DATA.map((el, index) =>
-                        <Card item={el} id={index}/>
-                    )
-                }
+    const [data, setData] = useState([
+        {name: "AppJam", color: "black", logo: AppJam_Logo, tag:"DFS Programs"},
+        {name: "WebJam", color: "red", logo: WebJam_Logo, tag:"DFS Programs"},
+        {name: "LeStem", color: "green", logo: Lestem_Logo, tag:"Public Schools"},
+        {name: "Engineering Inventors", color: "purple", logo: Efk_Logo, tag:"Non-profit Partners"},
+        {name: "Scratch", color: "yellow", logo: Scratch_Logo, tag:"Housing Community"}
+    ]);
+    const [viewType, setViewType] = useState("Programs");
+    const [filterType, setFilterType] = useState("All");
+
+    function getViewType(type) {
+        setViewType(type);
+        if(type === "Programs") {
+            setData( [
+                {name: "AppJam", color: "black", logo: AppJam_Logo, tag:"DFS Programs"},
+                {name: "WebJam", color: "red", logo: WebJam_Logo, tag:"DFS Programs"},
+                {name: "LeStem", color: "green", logo: Lestem_Logo, tag:"Public Schools"},
+                {name: "Engineering Inventors", color: "purple", logo: Efk_Logo, tag:"Non-profit Partners"},
+                {name: "Scratch", color: "yellow", logo: Scratch_Logo, tag:"Housing Community"}
+                ]);
+        } else{
+            setData( [
+                {name: "Edison Elementary School", color: "black", logo: "https://www.gusd.net/cms/lib/CA01000648/Centricity/ModuleInstance/1993/large/Edison%20Elementary%20Front.jpg", tag:"Public Schools"},
+                {name: "Alta Charter School", color: "red", logo: "https://schoolmint-assets.s3.amazonaws.com/lausdschoolsearch/upload/p4y6x1_Alta%20California.jpg", tag:"Private Schools"},
+                {name: "Turner High School", color: "green", logo: "https://www.turnerconstruction.com/Files/ProjectImage?url=%2Fsites%2Fmarketingstories%2FMarketing%2520Story%2520Images%2Foriginal.8d0ac363-e0cd-4911-8ea0-e598682a52fa.jpg&width=707&height=470&crop=True&jpegQuality=95", tag:"Public Schools"},
+                {name: "Berkley High School", color: "purple", logo: "https://i0.wp.com/www.dailycal.org/assets/uploads/2018/08/BHS_ChenGong_file-698x450.jpg?ssl=1", tag:"Public Schools"},
+                {name: "Irvine Community Center", color: "purple", logo: "https://ei.marketwatch.com/Multimedia/2019/05/20/Photos/ZQ/MW-HJ914_BNIRBe_20190520150242_ZQ.jpg?uuid=d8a3b3c4-7b31-11e9-bf02-9c8e992d421e", tag:"Housing Community"},
+                {name: "Bloom Foundation", color: "purple", logo: "https://images.squarespace-cdn.com/content/v1/5dcc60ee9b50f1429aceb553/1580465469292-HES155P0ZF86BCBT9J19/ke17ZwdGBToddI8pDm48kBev1jCTHhfMggHZeq8iG4BZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpw--afqNaTtD4llbkIP7lK59WKqKDVq6YNZmIrRfNqow9Qde5juP5k1GMq7i4XZu60/Bloom_Partners_Artboard%2B20.png", tag:"Non-profit Partners"}
+            ]);
+        }
+    }
+
+    function getfilterType(type){
+        setFilterType(type)
+    }
+
+    return (
+        <div className="programs_page">
+            <div className={"program-gallery_container"}>
+                <OptionsBar viewType={getViewType} filterType={getfilterType}/>
+                <div className={'program-gallery'}>
+                    {
+                        data.map((el, index) =>
+                            <Card item={el} id={index} viewType={viewType} filterType={filterType}/>
+                        )
+                    }
+                </div>
+            </div>
+            <div className={"program-side-info_container"}>
+
+
             </div>
         </div>
-        <div className={"program-side-info_container"}>
-
-
-        </div>
-    </div>
-  );
+    );
 }
+
 export default Programs;
