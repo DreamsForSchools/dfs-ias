e4a36777a4f79a12b60b7df2f9951b4058169b32
Mars <32443134+uci-mars@users.noreply.github.com>
2021-03-03 18:21:56 -0800

    move util function to its own file

--- client/src/pages/InstructorsPage/InstructorsSideInfo.jsx
+++ client/src/pages/InstructorsPage/InstructorsSideInfo.jsx
@@ -4,41 +4,7 @@ import { PencilSquare, CalendarWeek, TelephoneFill, X, Check } from 'react-boots
 import { Button, OverlayTrigger, Popover, Badge } from 'react-bootstrap';
 import Dot from "../../components/Dot";
 import avatar from '../../assets/avatar.png';
-
-const formatPhoneNumber = (number) => {
-    return `(${number.substring(0, 3)}) ${number.substring(3, 6)}-${number.substring(6, 10)}`;
-}
-
-const formatMilitaryTime = (time) => {
-    let hour = Number(time.substring(0, 2));
-    let suffix = hour >= 12 ? "pm":"am";
-    let standardHour = (hour + 11) % 12 + 1;
-
-    return `${standardHour}:${time.substring(3,5)} ${suffix}`;
-}
-
-const formatAvailability = (availability) => {
-    let bucket = {};
-    let dateKey = {1: 'M', 2: 'Tu', 3: 'W', 4: 'Th', 5: 'F'};
-    let result = [];
-
-    availability.forEach((slot) => {
-        let timeAvailable = `${formatMilitaryTime(slot.startTime)} - ${formatMilitaryTime(slot.endTime)}`;
-        if (!bucket.hasOwnProperty(timeAvailable)) {
-            bucket[timeAvailable] = [slot.weekday];
-        } else {
-            bucket[timeAvailable].push(slot.weekday);
-        }
-    })
-
-    Object.keys(bucket).forEach((key) => {
-        let dates = '';
-        bucket[key].forEach((d) => dates += dateKey[d]);
-        result.push(`${dates} ${key}`)
-    })
-
-    return(result);
-}
+import { formatAvailability, formatPhoneNumber } from "../../util/formatData";
 
 const InstructorsSideInfo = (props) => {
     const { instructor, programsColorKey } = props;
