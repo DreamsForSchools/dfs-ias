b1271b699c4acacd34274008369d0532b00853a9
trishaprile <trishaprilee@gmail.com>
2021-03-04 13:14:59 -0800

    small fixes

--- client/src/pages/InstructorsPage/Toolbar.jsx
+++ client/src/pages/InstructorsPage/Toolbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./Toolbar.scss";
 import {InputGroup, FormControl, Form, Button} from 'react-bootstrap';
 import { Search } from 'react-bootstrap-icons';
 
@@ -9,14 +10,15 @@ const Toolbar = () => {
                 <FormControl
                     placeholder="Search"
                     aria-label="Search"
+                    class-name="searh-bar"
                 />
                 <InputGroup.Append>
                     <Button variant="primary" className="search-btn"><Search /></Button>
                 </InputGroup.Append>
             </InputGroup>
-
-            <Form.Label>View by: </Form.Label>
-            <Form.Control as="select">
+            
+            <Form.Label className="view-by">View by: </Form.Label>
+            <Form.Control as="select" className="view-select">
                 <option>1</option>
                 <option>2</option>
                 <option>3</option>
