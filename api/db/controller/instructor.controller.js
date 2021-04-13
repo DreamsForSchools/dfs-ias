c204faa5ea9bde752c0e24d8805c7c02d7f565a4
Michelle Wang <zhiyimichelle@gmail.com>
2021-04-13 03:08:55 -0700

    updated instructor

--- api/db/controller/instructor.controller.js
+++ api/db/controller/instructor.controller.js
@@ -25,13 +25,12 @@ exports.createSingle = function(req, res) {
 };
 
 exports.createCSV = function(req, res) {
-    const new_instructor = new Instructor(req.body);
     //handles null error
     if (req.body.constructor === Object && Object.keys(req.body).length === 0){
         res.status(400).send({ error:true, message: 'Please provide all required field' });
     }
     else {
-        Instructor.createCSV(new_instructor, function(err, instructor) {
+        Instructor.createCSV(req.body, function(err, instructor) {
             if (err)
                 res.send(err);
             else res.json({error:false, message:"Instructor added successfully!",data:instructor});
