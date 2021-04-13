9d8486511875e9a14d74a148430ff178b46afdc8
trishaprile <trishaprilee@gmail.com>
2021-04-13 12:52:46 -0700

    sorter dnd

--- client/src/pages/SorterPage/Sidebar.jsx
+++ client/src/pages/SorterPage/Sidebar.jsx
@@ -1,50 +1,79 @@
-import React, { useCallback, useReducer } from "react";
+import React, { createContext } from "react";
 import "./Sidebar.scss";
 import Instructor from './Instructor.jsx';
 import { InputGroup, FormControl, Button } from "react-bootstrap";
 import { Search, PlusCircle } from 'react-bootstrap-icons';
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import { Droppable, Draggable } from 'react-beautiful-dnd';
 import { getRandomInstructorSet } from "../../util/sampleData";
-import produce from "immer";
-import Dot from "../../components/Dot";
-import { formatAvailability } from "../../util/formatData";
+// import produce from "immer";
+// import Dot from "../../components/Dot";
+// import { formatAvailability } from "../../util/formatData";
 
-const dragReducer = produce((draft, action) => {
-  switch (action.type) {
-    case "MOVE": {
-      draft[action.from] = draft[action.from] || [];
-      draft[action.to] = draft[action.to] || [];
-      const [removed] = draft[action.from].splice(action.fromIndex, 1);
-      draft[action.to].splice(action.toIndex, 0, removed);
-    }
-  }
-});
+// const dragReducer = produce((draft, action) => {
+//   switch (action.type) {
+//     case "MOVE": {
+//       draft[action.from] = draft[action.from] || [];
+//       draft[action.to] = draft[action.to] || [];
+//       const [removed] = draft[action.from].splice(action.fromIndex, 1);
+//       draft[action.to].splice(action.toIndex, 0, removed);
+//     }
+//   }
+// });
 
-function Sidebar() {
-  const [state, dispatch] = useReducer(dragReducer, { items: getRandomInstructorSet(10), });
+const Sidebar = () => {
+  // const [state, dispatch] = useReducer(dragReducer, { items: getRandomInstructorSet(10), });
+  const { searchedCourses, setSearchedCourses } = createContext();
+  const { planData } = createContext();
 
-  const programsColorKey = {
-    "AppJam": "#BB6BD9",
-    "WebJam": "#40CCC8",
-    "LESTEM": "#F2994A",
-    "Engineering Inventors": "#4B4B92",
-    "Scratch": "#F2C94C"
-  };
+  // const programsColorKey = {
+  //   "AppJam": "#BB6BD9",
+  //   "WebJam": "#40CCC8",
+  //   "LESTEM": "#F2994A",
+  //   "Engineering Inventors": "#4B4B92",
+  //   "Scratch": "#F2C94C"
+  // };
 
-  const onDragEnd = useCallback((result) => {
-    if (result.reason === "DROP") {
-      if (!result.destination) {
-        return;
-      }
-      dispatch({
-        type: "MOVE",
-        from: result.source.droppableId,
-        to: result.destination.droppableId,
-        fromIndex: result.source.index,
-        toIndex: result.destination.index,
-      });
-    }
-  }, []);
+  // const onDragEnd = useCallback((result) => {
+  //   if (result.reason === "DROP") {
+  //     if (!result.destination) {
+  //       return;
+  //     }
+  //     dispatch({
+  //       type: "MOVE",
+  //       from: result.source.droppableId,
+  //       to: result.destination.droppableId,
+  //       fromIndex: result.source.index,
+  //       toIndex: result.destination.index,
+  //     });
+  //   }
+  // }, []);
+
+  const resultList = getRandomInstructorSet(10).map((instructor, index) => {
+    return (
+      <Draggable
+          key={instructor.id}
+          draggableId={instructor.id}
+          index={index}
+      >
+        {(provided) => (
+          <div
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            ref={provided.innerRef}
+          >
+            <Instructor
+              firstName={instructor.firstName}
+              lastName={instructor.lastName}
+              car={instructor.hasCar}
+              returnee={instructor.previouslyTaught}
+              pref={instructor.pref}
+              availability={instructor.availability}
+            />
+          </div>
+        )}
+      </Draggable>
+    )
+  });
 
   return (
     <div className="sidebar">
@@ -61,8 +90,20 @@ function Sidebar() {
       </InputGroup>
       <Button className="filter-btn">Filter</Button>
       <Button className="add-instructor-btn"><PlusCircle /> Add Instructor</Button>
+
+      <Droppable droppableId="search-result">
+        {(provided) => (
+            <div
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+            >
+              {resultList}
+              {provided.placeholder}
+            </div>
+        )}
+      </Droppable>
       
-      <DragDropContext onDragEnd={onDragEnd}>
+      {/* <DragDropContext onDragEnd={onDragEnd}>
         <Droppable droppableId="items" type="INSTRUCTOR">
           {(provided) => {
             return (
@@ -115,9 +156,9 @@ function Sidebar() {
             );
           }}
         </Droppable>
-      </DragDropContext>
+      </DragDropContext> */}
     </div>
   );
-};
+}
 
 export default Sidebar;
--- client/src/pages/SorterPage/Sorter.jsx
+++ client/src/pages/SorterPage/Sorter.jsx
@@ -1,36 +1,93 @@
-import React from 'react';
+import React, { createContext, useContext } from 'react';
 import './Sorter.scss';
 import Sidebar from './Sidebar.jsx';
 import ProgramModule from './ProgramModule.jsx';
+import MainPanel from './MainPanel.jsx';
+import { DragDropContext } from 'react-beautiful-dnd';
 
-function Sorter() {
+const getQuarterCourses = (droppableId, planData) => {
+  for (let i = 0; i < planData.length; i++) {
+    if (Array.isArray(planData[i][droppableId])) {
+      return planData[i][droppableId];
+    }
+  }
+}
+
+const Sorter = () => {
+  const { planData, setPlanData } = createContext();
+  const { searchedCourses } = createContext();
+
+  const onDragEnd = (result) => {
+    const { source, destination } = result;
+
+    if (!destination) return;
+    if (destination.droppableId === 'search-result') return;
+
+    if (source.droppableId === 'search-result') {
+        // moved from search result to one of the quarters
+        const newPlanData = [...planData];
+        const newSearchedCourses = [...searchedCourses];
+        const quarterCourses = getQuarterCourses(destination.droppableId, newPlanData);
+
+        const [removed] = newSearchedCourses.splice(source.index, 1);
+        quarterCourses.splice(destination.index, 0, removed);
+
+        setPlanData(newPlanData);
+    } else if (source.droppableId !== 'search-result' && source.droppableId !== destination.droppableId) {
+        // moved from one quarter to the other
+        
+        const newPlanData = [...planData];
+        const fromQuarterCourses = getQuarterCourses(source.droppableId, newPlanData);
+        const toQuarterCourses = getQuarterCourses(destination.droppableId, newPlanData);
+
+        const [removed] = fromQuarterCourses.splice(source.index, 1);
+        toQuarterCourses.splice(destination.index, 0, removed);
+
+        setPlanData(newPlanData);
+    } else if (source.droppableId === destination.droppableId) {
+        // moved to the same quarter -> reorder
+        const newPlanData = [...planData];
+        const quarterCourses = getQuarterCourses(source.droppableId, newPlanData);
+
+        const [removed] = quarterCourses.splice(source.index, 1);
+        quarterCourses.splice(destination.index, 0, removed);
+
+        setPlanData(newPlanData);
+    }
+  };
+
   return (
     <div className="sorter">
-      <div className="main-wrapper">
-        <ProgramModule
-          name="AppJam"
-          color="#BB6BD9"
-        />
-        <ProgramModule
-          name="LESTEM"
-          color="#F2994A"
-        />
-        <ProgramModule
-          name="Scratch"
-          color="#F2C94C"
-        />
-        <ProgramModule
-          name="WebJam"
-          color="#40CCC8"
-        />
-        <ProgramModule
-          name="Engineering Inventors"
-          color="#4B4B92"
-        />
-      </div>
-      <div className="sidebar-wrapper">
-        <Sidebar />
-      </div>
+      <DragDropContext
+        onDragEnd={onDragEnd}
+      >
+        <div className="main-wrapper">
+          {/* <MainPanel /> */}
+          <ProgramModule
+            name="AppJam"
+            color="#BB6BD9"
+          />
+          <ProgramModule
+            name="LESTEM"
+            color="#F2994A"
+          />
+          <ProgramModule
+            name="Scratch"
+            color="#F2C94C"
+          />
+          <ProgramModule
+            name="WebJam"
+            color="#40CCC8"
+          />
+          <ProgramModule
+            name="Engineering Inventors"
+            color="#4B4B92"
+          />
+        </div>
+        <div className="sidebar-wrapper">
+          <Sidebar />
+        </div>
+      </DragDropContext>
     </div>
 
   );
