b62175ec1fd9c14dfa0b0295b627b8f1b3ff1a57
trishaprile <trishaprilee@gmail.com>
2021-04-20 03:19:36 -0700

    set up filter modal

--- client/src/pages/SorterPage/Sidebar/InstructorSearchForm.jsx
+++ client/src/pages/SorterPage/Sidebar/InstructorSearchForm.jsx
@@ -1,11 +1,13 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
+import './InstructorSearchForm.scss';
 import { AppContext } from '../AppContextProvider';
 import { getRandomInstructorSet } from "../../../util/sampleData";
-import { InputGroup, FormControl, Button } from "react-bootstrap";
+import { InputGroup, FormControl, Button, Modal, Form } from "react-bootstrap";
 import { Search } from 'react-bootstrap-icons';
 
 const InstructorSearchForm = ({setIsLoading}) => {
   const { setSearchedInstructors } = useContext(AppContext);
+  const [showFilter, setShowFilter] = useState();
 
   const onFormSubmit = async (e) => {
     e.preventDefault();
@@ -14,6 +16,9 @@ const InstructorSearchForm = ({setIsLoading}) => {
     setIsLoading(false);
     setSearchedInstructors(result);
   }
+
+  const handleShowFilter = () => setShowFilter(true);
+  const handleCloseFilter = () => setShowFilter(false);
 
   return (
     <div className="instructor-search-form">
@@ -27,8 +32,55 @@ const InstructorSearchForm = ({setIsLoading}) => {
           <Button variant="primary" className="search-btn" onClick={onFormSubmit}><Search /></Button>
         </InputGroup.Append>
       </InputGroup>
-      <Button className="filter-btn">Filter</Button>
+      <Button className="filter-btn" onClick={handleShowFilter}>Filter</Button>
       <Button className="auto-assign-btn">Auto-assign</Button>
+
+      <Modal dialogClassName="filter" show={showFilter} onHide={handleCloseFilter} onExited={handleCloseFilter} >
+          <Modal.Header closeButton style={{padding: '2rem 3rem 0 3rem', border: '0'}}>
+            <Modal.Title style={{fontSize: '36px', fontWeight: 'bold'}}>Filters</Modal.Title>
+          </Modal.Header>
+          <Modal.Body style={{padding: '1rem 3rem'}}>
+            <h5>Returnee</h5>
+            <Form.Group className="filter-group">
+              <Form.Check type="checkbox" label="Yes" />
+              <Form.Check type="checkbox" label="No" />
+            </Form.Group>
+            <h5>Owns a car</h5>
+            <Form.Group className="filter-group">
+              <Form.Check type="checkbox" label="Yes" />
+              <Form.Check type="checkbox" label="No" />
+            </Form.Group>
+            <h5>American Sign Language</h5>
+            <Form.Group className="filter-group">
+              <Form.Check type="checkbox" label="Yes" />
+              <Form.Check type="checkbox" label="No" />
+            </Form.Group>
+            <h5>Availability</h5>
+            <Form.Group className="filter-group">
+              <Form.Check type="checkbox" label="Monday" />
+              <Form.Check type="checkbox" label="Tuesday" />
+              <Form.Check type="checkbox" label="Wednesday" />
+              <Form.Check type="checkbox" label="Thursday" />
+              <Form.Check type="checkbox" label="Friday" />
+            </Form.Group>
+            <h5>Preferences</h5>
+            <Form.Group className="filter-group">
+              <Form.Check type="checkbox" label="AppJam" />
+              <Form.Check type="checkbox" label="WebJam" />
+              <Form.Check type="checkbox" label="LESTEM" />
+              <Form.Check type="checkbox" label="Scratch" />
+              <Form.Check type="checkbox" label="Engineering Inventors" />
+            </Form.Group>
+            <h5>Programming Language</h5>
+            <h5>Spoken Language</h5>
+            <h5>Year</h5>
+            <h5>Major</h5>
+            <div className="filter-btns">
+              <Button className="apply-btn" onClick={handleCloseFilter}>Apply Filters</Button>
+              <span className="reset-btn">Reset Filters</span>
+            </div>
+          </Modal.Body>
+      </Modal>
     </div>
   );
 }
