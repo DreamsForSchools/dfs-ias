03b53db4af47082d3ae3ef1b4b89a83e5b92d663
trishaprile <trishaprilee@gmail.com>
2021-04-20 13:02:41 -0700

    auto-assign confirmation dialog

--- client/src/pages/SorterPage/Sidebar/InstructorSearchForm.jsx
+++ client/src/pages/SorterPage/Sidebar/InstructorSearchForm.jsx
@@ -8,8 +8,9 @@ import { Search } from 'react-bootstrap-icons';
 const InstructorSearchForm = ({setIsLoading}) => {
   const { setSearchedInstructors } = useContext(AppContext);
   const [showFilter, setShowFilter] = useState();
+  const [showAutoAssignConfirmation, setShowAutoAssignConfirmation] = useState();
 
-  const onFormSubmit = async (e) => {
+  const onSearchSubmit = async (e) => {
     e.preventDefault();
     setIsLoading(true);
     let result = getRandomInstructorSet(10);
@@ -19,6 +20,8 @@ const InstructorSearchForm = ({setIsLoading}) => {
 
   const handleShowFilter = () => setShowFilter(true);
   const handleCloseFilter = () => setShowFilter(false);
+  const handleShowAutoAssignConfirmation = () => setShowAutoAssignConfirmation(true);
+  const handleCloseAutoAssignConfirmation = () => setShowAutoAssignConfirmation(false);
 
   return (
     <div className="instructor-search-form">
@@ -29,11 +32,11 @@ const InstructorSearchForm = ({setIsLoading}) => {
           className="search-bar"
         />
         <InputGroup.Append>
-          <Button variant="primary" className="search-btn" onClick={onFormSubmit}><Search /></Button>
+          <Button variant="primary" className="search-btn" onClick={onSearchSubmit}><Search /></Button>
         </InputGroup.Append>
       </InputGroup>
       <Button className="filter-btn" onClick={handleShowFilter}>Filter</Button>
-      <Button className="auto-assign-btn">Auto-assign</Button>
+      <Button className="auto-assign-btn" onClick={handleShowAutoAssignConfirmation}>Auto-assign</Button>
 
       <Modal dialogClassName="filter" show={showFilter} onHide={handleCloseFilter} onExited={handleCloseFilter} >
           <Modal.Header closeButton style={{padding: '2rem 3rem 0 3rem', border: '0'}}>
@@ -80,6 +83,19 @@ const InstructorSearchForm = ({setIsLoading}) => {
               <span className="reset-btn">Reset Filters</span>
             </div>
           </Modal.Body>
+      </Modal>
+
+      <Modal centered show={showAutoAssignConfirmation} onHide={handleCloseAutoAssignConfirmation} onExited={handleCloseAutoAssignConfirmation} >
+        <Modal.Header closeButton style={{padding: '2rem 3rem 0 3rem', border: '0'}}>
+          <Modal.Title style={{fontSize: '24px'}}>Are you sure you want to auto-assign instructors?</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          This action will automatically sort all unassigned instructors and any instructors that are not locked.
+        </Modal.Body>
+        <Modal.Footer style={{border: '0'}}>
+          <Button variant="light" onClick={handleCloseAutoAssignConfirmation}>Cancel</Button>
+          <Button variant="primary" onClick={handleCloseAutoAssignConfirmation}>Confirm</Button>
+        </Modal.Footer>
       </Modal>
     </div>
   );
