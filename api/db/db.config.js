a557075b187020cc67e8fb90095ddc6f4941a388
Mars <32443134+uci-mars@users.noreply.github.com>
2021-04-19 17:44:09 -0700

    update config to connect to cloud db

--- api/db/db.config.js
+++ api/db/db.config.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
 var db = mysql.createPool({
     connectionLimit: 100,
-    host: 'db',
-    user: 'root',
-    password: 'root',
-    database: 'dfs',
+    host: process.env.CLOUD_DB_IP,
+    user: process.env.CLOUD_DB_DEV_USERNAME,
+    password: process.env.CLOUD_DB_DEV_PASSWORD,
+    database: process.env.CLOUD_DB_NAME,
     debug: false
 });
 
