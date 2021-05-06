ed13b0451804154a72e2e60d64690034914c822a
Michelle Wang <zhiyimichelle@gmail.com>
2021-05-05 18:07:24 -0700

    partnerLocationCache start

--- api/db/model/partner.model.js
+++ api/db/model/partner.model.js
@@ -16,7 +16,12 @@ var Partner = function(partner) {
 Partner.create = function (newPartner, result) {
     db.query("INSERT INTO partners set ?", newPartner, function (err, res){
         if (err) result(err, null);
-        else result(null, res);
+        else {
+            let insertedPartnerId = res.insertId;
+            let address = newPartner.street + ", " + newPartner.city + ", " + newPartner.state + " " + newPartner.zip;
+            locationCacheCheck(newPartner.name,address,insertedPartnerId,result);
+            result(null, res);
+        }
     })
 }
 
@@ -50,5 +55,77 @@ Partner.updateById = function (id, partner, result) {
             else result(null, res);
     })
 }
+
+function locationCacheCheck(partnerName, partnerLocation, insertedPartnerID,result){
+    db.query("SELECT * FROM locationCache WHERE name = ?", instructorUniversity, function(err,res){
+        if(err) result(err,null)
+        if(res.length >=1 ){    //if location cache exists, copy data over with new instructorId
+            let location = res[0];
+            location.partnerId = insertedPartnerID;
+            delete location['id'];
+            insertLocation(insertedPartnerID,location,result);
+        }else{                  //new gmap
+            let location = {};
+            location.name = partnerName;
+            axios.get(`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key=${process.env.GMAP_API_KEY}&inputtype=textquery&input=${partnerLocation}&inputtype=textquery&fields=geometry,photos,name,formatted_address,place_id`)
+                .then((response) => {
+                    if(response.data.status == 'OK' && response.data.candidates.length >= 1){
+                        location.address = response.data.candidates[0].formatted_address;
+                        location.latitude = response.data.candidates[0].geometry.location.lat;
+                        location.longititude = response.data.candidates[0].geometry.location.lng;
+                        location.placeId = response.data.candidates[0].place_id;
+                        if(response.data.candidates[0].photos.length >= 1)
+                            location.image = response.data.candidates[0].photos[0].photo_reference;
+                        let date = new Date();
+                        axios.get(`https://maps.googleapis.com/maps/api/timezone/json?location=${location.latitude},${location.longititude}&timestamp=${Math.floor(date.getTime()/1000)}&key=${process.env.GMAP_API_KEY}`)
+                            .then((response) => {
+                                location.rawOffset = response.data.rawOffset;
+                                location.dstOffset = response.data.dstOffset;
+                                db.query("INSERT INTO locationCache set ?",location,function(err,res){ //insert location cache w/o instructor id
+                                    if(err) result(err,null);
+                                    else{
+                                        //insert into location cache WITH instructor ID
+                                        location.partnerId = insertedPartnerID;
+                                        insertLocation(insertedPartnerID,location,result);
+                                    }
+                                });
+
+                            });
+                    }else{
+                        result(err,null);
+                    }
+                });
+        }
+    });
+}
+
+//insert location with partnerID
+function insertLocation(insertedPartnerID,location,result)
+{
+    db.query("SELECT * FROM locationCache WHERE partnerId = ?",insertedPartnerID,function(err,res){
+        if(err) result(err,null);
+        else{
+            if(res.length > 0 && res.name != location.name){    //instructor already exists, update
+                db.query("UPDATE locationCache SET name = ?, image = ?, address = ?, district = ?, longititude = ?, latitude = ?, rawOffset = ?, dstOffset = ?, partnerId = ?, placeId = ?, WHERE instructorId = ?",
+                    [location.name, location.image, location.address, location.district, location.longititude, location.latitude, location.rawOffset, location.dstOffset, location.partnerId, location.placeId, insertedInstructorID],
+                    function(err,res){
+                        if(err) result(err,null);
+                    });
+            }else{
+                 //inserting location with instructor id
+                 
+                 delete location["locationCacheId"];
+                 console.log(location);
+                db.query("INSERT INTO locationCache set ?",location,function(err,res){
+                    if(err) result(err,null);
+                });
+
+            }
+        }
+
+    });
+}
+
+
 
 module.exports = Partner;
