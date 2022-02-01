f87c10a8887a78a133555076768720bdf42c8d39
Jules Labador <juleslabador@gmail.com>
2022-01-31 17:46:37 -0800

    Began adding documentation for Class.jsx

--- client/src/pages/SorterPage/Main/Class.jsx
+++ client/src/pages/SorterPage/Main/Class.jsx
@@ -5,30 +5,47 @@ import Instructor from '../Instructor.jsx';
 import { CalendarWeek, People, LockFill, UnlockFill } from 'react-bootstrap-icons';
 import { formatAvailability } from "../../../util/formatData";
 
+/**
+ * 
+ * @param {id} - Class ID?
+ * @param {partner} - Associated partner? 
+ * @param {time} - Set time for when the class is set to start?
+ * @param {instructorsNeeded} - # of instructors needed
+ * @param {instructors} - # of instructors added
+ * @param {programId}
+ * @param {state} - ???
+ * @param {parentLockStatus} - ??
+ * 
+ */
 const Class = ({ id, partner, time, instructorsNeeded, instructors, programId, state, parentLockStatus}) => {
+
   const [numInstructors, setNumInstructors] = useState(0);
   const [lock, setLock] = useState(false);
 
-
-    const handleLock = () => {
-        setLock(true);
-    }
-    const handleUnlock = () => {
-        setLock(false);
-    }
+  const handleLock = () => {
+      setLock(true);
+  }
+  const handleUnlock = () => {
+      setLock(false);
+  }
 
   useEffect(() => {
+
+      // What is 'val' supposed to be?
+      //val is false if the instructor is not in the lockedInstructors 
+      // lockedInstructors is instructors locked in a section/class
       let val = true;
-      if(instructors){
-          for(const instructor of instructors){
-              if(!state["lockedInstructors"].includes(instructor.instructorId)){
+      if (instructors) {
+          for (const instructor of instructors) {
+              if (!state["lockedInstructors"].includes(instructor.instructorId)) {
                   val = false;
               }
           }
       }
+
       let count = (instructors ? instructors.length : 0);
       setNumInstructors(count);
-      if(count === 0){
+      if(count === 0){ 
           setLock(false);
       } else if(parentLockStatus === true || val) {
           setLock(true);
@@ -36,7 +53,7 @@ const Class = ({ id, partner, time, instructorsNeeded, instructors, programId, s
           setLock(false);
       }
 
-  }, [parentLockStatus,setNumInstructors, instructors])
+  }, [parentLockStatus, setNumInstructors, instructors])
 
   return (
     <div className="class">
