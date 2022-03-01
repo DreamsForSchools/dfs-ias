1183a5876a6ecb6aacd3212551cc4a61a8683879
Michael Wijangco <mwijangc@uci.edu>
2022-02-28 19:04:00 -0800

    attempted row implementation
    made new component, tested prop deconstruction, added to modal!

---
+++ client/src/pages/SorterPage/Main/AssignInstructorsTable.jsx
@@ -0,0 +1,111 @@
+import React from 'react';
+import { Table, Badge, OverlayTrigger, Popover } from 'react-bootstrap';
+import Dot from '../../../design-system/dots';
+
+// const InstructorsRow = (props) => {
+//     const {
+//         programsColorKey,
+//         onClick,
+//         instructor
+//     } = props;
+
+
+//     const {
+//         firstName,
+//         lastName,
+//         isActive,
+//         year,
+//         major,
+//         university,
+//         firstPref,
+//         secondPref,
+//         thirdPref,
+//         fourthPref,
+//         schoolYear
+//     } = instructor;
+
+
+//     return (
+//         // put onclick funct for the tr
+//         <tr className={"instructor_row"} >
+//             <td>{firstName + " " + lastName}</td>
+//             <td>{schoolYear}</td>
+//             <td>{major}</td>
+//             <td>{university}</td>
+//             <td>
+//                 <div style={{display: "inline-block"}}>
+//                     {/* <OverlayTrigger
+//                         placement="right"
+//                         delay={{ show: 250, hide: 400 }}
+//                         overlay={
+//                             <Popover>
+//                                 <Popover.Title as="h3">{`Program Preferences`}</Popover.Title>
+//                                 <Popover.Content>
+//                                     {
+//                                         [firstPref, secondPref, thirdPref, fourthPref].map((el, idx) =>
+//                                             programsColorKey[el] && <h6 key={idx}>
+//                                                 <Dot color={programsColorKey[el]} />
+//                                                 <span style={{
+//                                                     paddingLeft: 8
+//                                                 }}>{el}</span>
+//                                             </h6>
+//                                         )
+//                                     }
+//                                 </Popover.Content>
+//                             </Popover>
+//                         }
+//                     > */}
+//                         {/* <div style={{padding: "0 1rem"}}>
+//                             {[firstPref, secondPref, thirdPref, fourthPref].map((el, idx) =>
+//                                 programsColorKey[el] && <Dot color={programsColorKey[el]} key={idx}/>
+//                             )}
+//                         </div>
+//                     </OverlayTrigger> */}
+//                 </div>
+//             </td>
+//         </tr>
+//     )
+// }
+
+
+
+const AssignInstructorsTable = (props) => {
+
+    const {
+        filteredInstructors
+    } = props;
+
+    return (
+        <div >
+            
+            <Table >
+                <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>School</th>
+                    <th>Other Languages</th>
+                    <th>Schedule</th>
+                    <th>Tags</th>
+                </tr>
+                </thead>
+                <tbody>
+                     {/* {
+                        console.log(filteredInstructors)
+                    } */}
+                    {props.filteredInstructors.map((el, idx) =>
+                        // <InstructorsRow
+                        //     instructor={el}
+                        //     key={idx}
+                        //     programsColorKey={props.programsColorKey}
+                            
+                        // />
+                        <p>{el.firstName} </p>
+                        
+                    )} 
+                </tbody>
+            </Table>
+        </div>
+    )
+}
+
+export default AssignInstructorsTable;
